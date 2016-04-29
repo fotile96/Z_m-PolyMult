3b8d1178585fafe9e6b49023483fd404d30c58eb
fotile96 <fotile13@gmail.com>
2016-04-29 13:17:19 +0800

    constant improvement

--- magic_number_gen.cpp
+++ magic_number_gen.cpp
@@ -7,15 +7,15 @@ typedef long long ll;
 const ll P = 1000000007LL;
 
 int main() {
-	double ans = P*P;
+	double ans = (double)P*P;
 	ll y;
-	for(int x=sqrt(P); x<P; x++) {
-		double na = max((double)x*x, (double)P/x*P/x*(P-(ll)x*x%P));
+	for(ll x=sqrt(P); x<P; x++) {
+		double na = max((double)x*x, (double)P/x/2*P/x/2*(P-(ll)x*x%P));
 		if(na < ans) {
 			ans = na;
 			y = x;
 		}
-		if(x*x > ans) break;
+		if((ll)x*x > ans) break;
 	}
 	printf("%lld\n%lld\n", y, P-(ll)y*y%P);
 	return 0;
